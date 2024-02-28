6e9f766f12c1d434fb0a4a0df23c455cbaf6d055
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-02-28 16:59:08 -0500

    finish importing gen 1 auth to gen 2 app

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,34 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const cfnUserPool = backend.auth.resources.cfnResources.cfnUserPool;
+
+// override values
+
+cfnUserPool.adminCreateUserConfig = undefined;
+
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 8,
+    requireLowercase: false,
+    requireNumbers: false,
+    requireSymbols: false,
+    requireUppercase: false,
+    temporaryPasswordValidityDays: undefined,
+    typeHint: undefined,
+  },
+};
+
+cfnUserPool.verificationMessageTemplate = undefined;
+
+cfnUserPool.accountRecoverySetting = undefined;
+
+cfnUserPool.mfaConfiguration = "OFF";
+
+cfnUserPool.smsVerificationMessage = undefined;
