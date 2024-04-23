537dae178c9f02b09e6c97a69acb0f4a9a83f2dd
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-23 12:21:57 -0400

    remove unnecessary code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,48 +2,7 @@
 import { Amplify } from "aws-amplify";
 import config from "@/amplifyconfiguration.json";
 
-Amplify.configure(
-  {
-    ...config,
-    Auth: {
-      Cognito: {
-        identityPoolId: config.aws_cognito_identity_pool_id,
-        userPoolClientId: config.aws_user_pools_web_client_id,
-        userPoolId: config.aws_user_pools_id,
-        allowGuestAccess: true,
-      },
-    },
-  },
-  { ssr: true }
-);
-
-// const config: ResourcesConfig = {
-//   Auth: {
-//     Cognito: {
-//       userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID as string,
-//       userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID as string,
-//     },
-//   },
-//   API: {
-//     GraphQL: {
-//       endpoint: "https://endpoint",
-//       region: "us-east-1",
-//       defaultAuthMode: "userPool",
-//     },
-//   },
-// };
-
-// export function configureAmplify(config: ResourcesConfig): void {
-//   if (!isConfigured) {
-//     Amplify.configure(config, { ssr: true });
-
-//     const amplifyConfigured = Amplify.getConfig();
-//     console.log("configured", amplifyConfigured);
-//     isConfigured = true;
-//   }
-// }
-
-// configureAmplify(config);
+Amplify.configure(config, { ssr: true });
 
 export default function RootLayout({
   children,
