6985328f6223e6251e61d4879aed798ef4fe9e14
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-23 16:24:12 -0400

    enable logging

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,18 +2,14 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { aws_events } from "aws-cdk-lib";
-import {
-  Effect,
-  PolicyDocument,
-  PolicyStatement,
-  Role,
-  ServicePrincipal,
-} from "aws-cdk-lib/aws-iam";
+import { aws_iam } from "aws-cdk-lib";
 
 export const backend = defineBackend({
   auth,
   data,
 });
+
+const apiStack = backend.createStack("MyLibraryStack");
 
 const eventStack = backend.createStack("MyExternalDataSources");
 
@@ -25,18 +21,42 @@ const eventBus = aws_events.EventBus.fromEventBusName(
 
 backend.data.addEventBridgeDataSource("EventBridgeDataSource", eventBus);
 
+backend.data.addNoneDataSource("NoneDataSource");
+
+// generate code for a cloudwatch logs role
+
+const cloudWatchLogsRole = new aws_iam.Role(
+  apiStack,
+  "MyLibraryCloudWatchRole",
+  {
+    roleName: "MyLibraryCloudWatchRole",
+    assumedBy: new aws_iam.ServicePrincipal("appsync.amazonaws.com"),
+    managedPolicies: [
+      aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSAppSyncPushToCloudWatchLogs"
+      ),
+    ],
+  }
+);
+
+backend.data.resources.cfnResources.cfnGraphqlApi.logConfig = {
+  cloudWatchLogsRoleArn: cloudWatchLogsRole.roleArn,
+  fieldLogLevel: "ALL",
+  excludeVerboseContent: false,
+};
+
 // Create the Policy Statement
-const policyStatement = new PolicyStatement({
-  effect: Effect.ALLOW,
+const policyStatement = new aws_iam.PolicyStatement({
+  effect: aws_iam.Effect.ALLOW,
   actions: ["appsync:GraphQL"],
   resources: [`${backend.data.resources.graphqlApi.arn}/types/Mutation/*`],
 });
 
 // Create the Role and attach the policy
-const eventBusRole = new Role(eventStack, "AppSyncInvokeRole", {
-  assumedBy: new ServicePrincipal("events.amazonaws.com"),
+const eventBusRole = new aws_iam.Role(eventStack, "AppSyncInvokeRole", {
+  assumedBy: new aws_iam.ServicePrincipal("events.amazonaws.com"),
   inlinePolicies: {
-    PolicyStatement: new PolicyDocument({
+    PolicyStatement: new aws_iam.PolicyDocument({
       statements: [policyStatement],
     }),
   },
@@ -48,11 +68,11 @@ const rule = new aws_events.CfnRule(eventStack, "MyOrderRule", {
   eventPattern: {
     source: ["amplify.orders"],
     ["detail-type"]: ["OrderStatusChange"],
-    detail: {
-      orderId: [{ exists: true }],
-      status: ["PENDING", "SHIPPED", "DELIVERED"],
-      message: [{ exists: true }],
-    },
+    // detail: {
+    //   orderId: [{ exists: true }],
+    //   status: ["PENDING", "SHIPPED", "DELIVERED"],
+    //   message: [{ exists: true }],
+    // },
   },
   targets: [
     {
