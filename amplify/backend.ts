3d6d82140a45238258725421e11c3e7fbe0fb0a9
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-05-02 10:48:36 -0400

    finalize working with files

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,37 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { storage } from "./storage/resource";
+import { auth } from "./auth/resource";
+import { BillingMode } from "aws-cdk-lib/aws-dynamodb";
 
-export const backend = defineBackend({
-  auth,
+const backend = defineBackend({
   data,
-  storage,
+  auth,
 });
+
+const dataResources = backend.data.resources;
+
+Object.values(dataResources.cfnResources.amplifyDynamoDbTables).forEach(
+  (table) => {
+    table.pointInTimeRecoveryEnabled = true;
+  }
+);
+
+dataResources.cfnResources.cfnGraphqlApi.xrayEnabled = true;
+
+dataResources.cfnResources.amplifyDynamoDbTables["Todo"].timeToLiveAttribute = {
+  attributeName: "ttl",
+  enabled: true,
+};
+
+dataResources.cfnResources.amplifyDynamoDbTables["Todo"].billingMode =
+  BillingMode.PROVISIONED;
+
+dataResources.cfnResources.amplifyDynamoDbTables["Todo"].provisionedThroughput =
+  {
+    readCapacityUnits: 5,
+    writeCapacityUnits: 5,
+  };
+
+dataResources.cfnResources.amplifyDynamoDbTables[
+  "Todo"
+].pointInTimeRecoveryEnabled = true;
